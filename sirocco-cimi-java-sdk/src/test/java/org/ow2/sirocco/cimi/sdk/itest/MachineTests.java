166ead0decd522ce2b66cd05e7b07754ca314770
Frederic Dang Tran <frederic.dangtran@orange.com>
2013-06-04 18:09:24 +0200

    Add Machine-related tests and clean-up System tests

---
+++ sirocco-cimi-java-sdk/src/test/java/org/ow2/sirocco/cimi/sdk/itest/MachineTests.java
@@ -0,0 +1,265 @@
+package org.ow2.sirocco.cimi.sdk.itest;
+
+import javax.ws.rs.core.MediaType;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.ow2.sirocco.cimi.sdk.Address;
+import org.ow2.sirocco.cimi.sdk.CimiClient;
+import org.ow2.sirocco.cimi.sdk.CimiClient.Options;
+import org.ow2.sirocco.cimi.sdk.CreateResult;
+import org.ow2.sirocco.cimi.sdk.Job;
+import org.ow2.sirocco.cimi.sdk.Machine;
+import org.ow2.sirocco.cimi.sdk.MachineConfiguration;
+import org.ow2.sirocco.cimi.sdk.MachineCreate;
+import org.ow2.sirocco.cimi.sdk.MachineImage;
+import org.ow2.sirocco.cimi.sdk.MachineNetworkInterface;
+import org.ow2.sirocco.cimi.sdk.MachineTemplate;
+
+public class MachineTests {
+    private static CimiClient client;
+
+    @BeforeClass
+    public static void readCimiProviderProperties() throws Exception {
+        String userName = java.lang.System.getProperty("test.userName");
+        String password = java.lang.System.getProperty("test.password");
+        String tenantId = java.lang.System.getProperty("test.tenant");
+        String cimiEndpointUrl = java.lang.System.getProperty("test.endpoint");
+        if (userName == null) {
+            throw new Exception("Missing test.userName property");
+        }
+        if (password == null) {
+            throw new Exception("Missing test.password property");
+        }
+        if (cimiEndpointUrl == null) {
+            throw new Exception("Missing test.endpoint property");
+        }
+        MachineTests.client = CimiClient.login(cimiEndpointUrl, userName, password, tenantId, Options.build().setDebug(true)
+            .setMediaType(MediaType.APPLICATION_XML_TYPE));
+    }
+
+    private Machine waitForMachineState(final String machineId, final int seconds, final Machine.State... expectedStates)
+        throws Exception {
+        int tries = seconds;
+        while (tries-- > 0) {
+            Machine machine = Machine.getMachineByReference(MachineTests.client, machineId);
+            if (machine.getState() == Machine.State.ERROR) {
+                throw new Exception("Machine state ERROR");
+            }
+            for (Machine.State expectedFinalState : expectedStates) {
+                if (machine.getState() == expectedFinalState) {
+                    return machine;
+                }
+            }
+            Thread.sleep(1000);
+        }
+        throw new Exception("Timeout waiting for Machine state transition");
+    }
+
+    private MachineImage waitForMachineImageState(final String machineImageId, final int seconds,
+        final MachineImage.State... expectedStates) throws Exception {
+        int tries = seconds;
+        while (tries-- > 0) {
+            MachineImage machineImage = MachineImage.getMachineImageByReference(MachineTests.client, machineImageId);
+            if (machineImage.getState() == MachineImage.State.ERROR) {
+                throw new Exception("MachineImage state ERROR");
+            }
+            for (MachineImage.State expectedFinalState : expectedStates) {
+                if (machineImage.getState() == expectedFinalState) {
+                    return machineImage;
+                }
+            }
+            Thread.sleep(1000);
+        }
+        throw new Exception("Timeout waiting for MachineImage state transition");
+    }
+
+    @Test
+    public void captureMachine() throws Exception {
+        Machine machine = null;
+        MachineImage capturedMachineImage = null;
+
+        try {
+            // create Machine
+
+            MachineConfiguration machineConfig = MachineConfiguration.getMachineConfigurations(MachineTests.client).get(0);
+            MachineImage machineImage = MachineImage.getMachineImages(MachineTests.client).get(0);
+            MachineTemplate machineTemplate = new MachineTemplate();
+            machineTemplate.setMachineConfigRef(machineConfig.getId());
+            machineTemplate.setMachineImageRef(machineImage.getId());
+            MachineCreate machineCreate = new MachineCreate();
+            machineCreate.setMachineTemplate(machineTemplate);
+            machineCreate.setName("MachineCaptureTest");
+            machineCreate.setDescription("machine capture test");
+
+            CreateResult<Machine> result = Machine.createMachine(MachineTests.client, machineCreate);
+            machine = result.getResource();
+            String machineId = result.getResource().getId();
+
+            machine = this.waitForMachineState(machineId, 30, Machine.State.STARTED, Machine.State.STOPPED);
+
+            // capture image
+
+            MachineImage newMachineImage = new MachineImage();
+            newMachineImage.setDescription("captured image from machine " + machineCreate.getName());
+            newMachineImage.setName("capturedMachineImage");
+            newMachineImage.setType(MachineImage.Type.IMAGE);
+            CreateResult<MachineImage> captureResult = machine.capture(newMachineImage);
+            capturedMachineImage = captureResult.getResource();
+
+            String capturedMachineImageId = captureResult.getResource().getId();
+
+            capturedMachineImage = this.waitForMachineImageState(capturedMachineImageId, 30, MachineImage.State.AVAILABLE);
+
+        } finally {
+            // Cleanup
+            Exception deleteException = null;
+
+            if (capturedMachineImage != null) {
+                try {
+                    Job job = capturedMachineImage.delete();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    deleteException = e;
+                    System.err.println("Failed to delete Machine Image " + capturedMachineImage.getId());
+                }
+            }
+            if (machine != null) {
+                try {
+                    if (machine.getState() == Machine.State.STARTED) {
+                        machine.stop();
+                        machine = this.waitForMachineState(machine.getId(), 30, Machine.State.STOPPED);
+                    }
+                    Job job = machine.delete();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    deleteException = e;
+                    System.err.println("Failed to delete Machine  " + machine.getId());
+                }
+            }
+            if (deleteException != null) {
+                throw deleteException;
+            }
+        }
+
+    }
+
+    @Test
+    public void restartMachine() throws Exception {
+        Machine machine = null;
+
+        try {
+            // create Machine
+
+            MachineConfiguration machineConfig = MachineConfiguration.getMachineConfigurations(MachineTests.client).get(0);
+            MachineImage machineImage = MachineImage.getMachineImages(MachineTests.client).get(0);
+            MachineTemplate machineTemplate = new MachineTemplate();
+            machineTemplate.setMachineConfigRef(machineConfig.getId());
+            machineTemplate.setMachineImageRef(machineImage.getId());
+            machineTemplate.setExtensionAttribute("realm", "UZXC0GRT-ZG8ZJCJ07-N-SECURE1");
+            MachineCreate machineCreate = new MachineCreate();
+            machineCreate.setMachineTemplate(machineTemplate);
+            machineCreate.setName("MachineWithRealmTest");
+            machineCreate.setDescription("machine with realm test");
+
+            CreateResult<Machine> result = Machine.createMachine(MachineTests.client, machineCreate);
+            machine = result.getResource();
+            String machineId = result.getResource().getId();
+
+            machine = this.waitForMachineState(machineId, 30, Machine.State.STARTED, Machine.State.STOPPED);
+
+            if (machine.getState() == Machine.State.STOPPED) {
+                machine.start();
+                this.waitForMachineState(machineId, 30, Machine.State.STARTED);
+            }
+
+            // restart machine
+
+            Job job = machine.restart(false);
+            this.waitForMachineState(machineId, 30, Machine.State.STARTED);
+
+        } finally {
+            // Cleanup
+            Exception deleteException = null;
+
+            if (machine != null) {
+                try {
+                    machine.stop();
+                    machine = this.waitForMachineState(machine.getId(), 30, Machine.State.STOPPED);
+                    Job job = machine.delete();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    deleteException = e;
+                    System.err.println("Failed to delete Machine  " + machine.getId());
+                }
+            }
+            if (deleteException != null) {
+                throw deleteException;
+            }
+        }
+    }
+
+    @Test
+    public void createMachineWithUserData() throws Exception {
+        Machine machine = null;
+
+        try {
+            // create Machine
+
+            MachineConfiguration machineConfig = MachineConfiguration.getMachineConfigurations(MachineTests.client).get(0);
+            MachineImage machineImage = MachineImage.getMachineImages(MachineTests.client).get(0);
+            MachineTemplate machineTemplate = new MachineTemplate();
+            machineTemplate.setMachineConfigRef(machineConfig.getId());
+            machineTemplate.setMachineImageRef(machineImage.getId());
+            machineTemplate.setUserData("1234567890");
+            MachineCreate machineCreate = new MachineCreate();
+            machineCreate.setMachineTemplate(machineTemplate);
+            machineCreate.setName("MachineUserDataTest");
+            machineCreate.setDescription("machine user data test");
+
+            CreateResult<Machine> result = Machine.createMachine(MachineTests.client, machineCreate);
+            machine = result.getResource();
+            String machineId = result.getResource().getId();
+
+            machine = this.waitForMachineState(machineId, 30, Machine.State.STARTED, Machine.State.STOPPED);
+
+            if (machine.getState() == Machine.State.STOPPED) {
+                machine.start();
+                this.waitForMachineState(machineId, 30, Machine.State.STARTED);
+            }
+
+            // get IP address(es) of the created machine
+
+            for (MachineNetworkInterface nic : machine.getNetworkInterfaces()) {
+                if (nic.getNetwork() != null) {
+                    System.out.println("Network type: " + nic.getNetwork().getNetworkType());
+                } else {
+                    System.out.println("NIC has no Network !!!");
+                }
+                for (Address addr : nic.getAddresses()) {
+                    System.out.println("IP: " + addr.getIp());
+                }
+            }
+
+        } finally {
+            // Cleanup
+            Exception deleteException = null;
+
+            if (machine != null) {
+                try {
+                    machine.stop();
+                    machine = this.waitForMachineState(machine.getId(), 30, Machine.State.STOPPED);
+                    Job job = machine.delete();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    deleteException = e;
+                    System.err.println("Failed to delete Machine  " + machine.getId());
+                }
+            }
+            if (deleteException != null) {
+                throw deleteException;
+            }
+        }
+    }
+
+}
--- sirocco-cimi-java-sdk/src/test/java/org/ow2/sirocco/cimi/sdk/itest/SystemTest.java
+++ sirocco-cimi-java-sdk/src/test/java/org/ow2/sirocco/cimi/sdk/itest/SystemTest.java
@@ -8,12 +8,16 @@ import javax.ws.rs.core.MediaType;
 import junit.framework.Assert;
 
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.ow2.sirocco.cimi.sdk.CimiClient;
 import org.ow2.sirocco.cimi.sdk.CimiClient.Options;
 import org.ow2.sirocco.cimi.sdk.CimiProviderException;
 import org.ow2.sirocco.cimi.sdk.ComponentDescriptor;
 import org.ow2.sirocco.cimi.sdk.CreateResult;
+import org.ow2.sirocco.cimi.sdk.CredentialTemplate;
+import org.ow2.sirocco.cimi.sdk.Job;
+import org.ow2.sirocco.cimi.sdk.Machine;
 import org.ow2.sirocco.cimi.sdk.MachineConfiguration;
 import org.ow2.sirocco.cimi.sdk.MachineImage;
 import org.ow2.sirocco.cimi.sdk.MachineTemplate;
@@ -21,27 +25,38 @@ import org.ow2.sirocco.cimi.sdk.Network;
 import org.ow2.sirocco.cimi.sdk.NetworkConfiguration;
 import org.ow2.sirocco.cimi.sdk.NetworkTemplate;
 import org.ow2.sirocco.cimi.sdk.QueryParams;
+import org.ow2.sirocco.cimi.sdk.System;
+import org.ow2.sirocco.cimi.sdk.SystemCreate;
+import org.ow2.sirocco.cimi.sdk.SystemMachine;
+import org.ow2.sirocco.cimi.sdk.SystemNetwork;
 import org.ow2.sirocco.cimi.sdk.SystemTemplate;
+import org.ow2.sirocco.cimi.sdk.SystemVolume;
+import org.ow2.sirocco.cimi.sdk.Volume;
+import org.ow2.sirocco.cimi.sdk.VolumeTemplate;
 
 public class SystemTest {
     private static CimiClient client;
 
     @BeforeClass
     public static void readCimiProviderProperties() throws Exception {
-        String userName = System.getProperty("test.userName");
-        String password = System.getProperty("test.password");
-        String cimiEndpointUrl = System.getProperty("test.endpoint");
+        String userName = java.lang.System.getProperty("test.userName");
+        String password = java.lang.System.getProperty("test.password");
+        String tenantId = java.lang.System.getProperty("test.tenant");
+        String cimiEndpointUrl = java.lang.System.getProperty("test.endpoint");
         if (userName == null) {
             throw new Exception("Missing test.userName property");
         }
         if (password == null) {
             throw new Exception("Missing test.password property");
         }
+        if (tenantId == null) {
+            throw new Exception("Missing test.tenantId property");
+        }
         if (cimiEndpointUrl == null) {
             throw new Exception("Missing test.endpoint property");
         }
-        SystemTest.client = CimiClient.login(cimiEndpointUrl, userName, password,
-            Options.build().setDebug(true).setMediaType(MediaType.APPLICATION_XML_TYPE));
+        SystemTest.client = CimiClient.login(cimiEndpointUrl, userName, password, tenantId, Options.build().setDebug(true)
+            .setMediaType(MediaType.APPLICATION_JSON_TYPE));
     }
 
     private ComponentDescriptor createMachineComponentDescriptor(final MachineConfiguration config, final MachineImage image,
@@ -68,15 +83,272 @@ public class SystemTest {
         return component;
     }
 
+    // @Test
+    // public void createSystemTemplate() throws Exception {
+    //
+    // MachineConfiguration config =
+    // MachineConfiguration.getMachineConfigurations(SystemTest.client,
+    // QueryParams.builder().filter("name='small'").build()).get(0);
+    // MachineImage image = MachineImage.getMachineImages(SystemTest.client,
+    // QueryParams.builder().filter("name='springoo'").build()).get(0);
+    // Network publicNetwork = Network.getNetworks(SystemTest.client,
+    // QueryParams.builder().filter("networkType='PUBLIC'").build()).get(0);
+    // NetworkConfiguration privateNetworkConfig =
+    // NetworkConfiguration.getNetworkConfigurations(SystemTest.client,
+    // QueryParams.builder().filter("networkType='PRIVATE'").build()).get(0);
+    //
+    // List<ComponentDescriptor> componentDescriptors = new
+    // ArrayList<ComponentDescriptor>();
+    //
+    // componentDescriptors.add(this.createMachineComponentDescriptor(config,
+    // image, publicNetwork, "LoadBalancer",
+    // "Apache load balancer"));
+    // componentDescriptors.add(this.createMachineComponentDescriptor(config,
+    // image, null, "Application Server",
+    // "JOnAS application server"));
+    // componentDescriptors.add(this.createMachineComponentDescriptor(config,
+    // image, null, "Database", "MySQL database"));
+    //
+    // NetworkTemplate privateNetworkTemplate = new NetworkTemplate();
+    // privateNetworkTemplate.setNetworkConfigRef(privateNetworkConfig.getId());
+    //
+    // ComponentDescriptor netComponentDescriptor = new ComponentDescriptor();
+    // netComponentDescriptor.setName("MyPrivateNetwork");
+    // netComponentDescriptor.setQuantity(1);
+    // netComponentDescriptor.setComponentTemplate(privateNetworkTemplate);
+    // netComponentDescriptor.setDescription("private network");
+    //
+    // componentDescriptors.add(netComponentDescriptor);
+    //
+    // SystemTemplate systemTemplate = new SystemTemplate();
+    // systemTemplate.setName("SpringooTemplateNew");
+    // systemTemplate.setDescription("Springoo multi-tier template");
+    //
+    // systemTemplate.setComponentDescriptors(componentDescriptors);
+    //
+    // CreateResult<SystemTemplate> result =
+    // SystemTemplate.createSystemTemplate(SystemTest.client, systemTemplate);
+    //
+    // String id = result.getResource().getId();
+    //
+    // systemTemplate =
+    // SystemTemplate.getSystemTemplateByReference(SystemTest.client, id);
+    //
+    // }
+
     @Test
-    public void createSystemTemplate() throws Exception {
+    public void cimiPrimerCreateSystemTemplate() throws Exception {
 
         MachineConfiguration config = MachineConfiguration.getMachineConfigurations(SystemTest.client).get(0);
         MachineImage image = MachineImage.getMachineImages(SystemTest.client).get(0);
+        CredentialTemplate credTemplate = null;
+        List<CredentialTemplate> credTemplates = CredentialTemplate.getCredentialTemplates(SystemTest.client, QueryParams
+            .builder().select("id").build());
+        if (credTemplates.isEmpty()) {
+            credTemplate = new CredentialTemplate();
+            // credTemplate.setExtensionAttribute("userName", "guest");
+            // credTemplate.setExtensionAttribute("password", "guest");
+            credTemplate.setExtensionAttribute("key", "1234");
+            credTemplate = CredentialTemplate.createCredentialTemplate(SystemTest.client, credTemplate).getResource();
+        } else {
+            credTemplate = credTemplates.get(0);
+        }
+
+        VolumeTemplate volumeTemplate = VolumeTemplate.getVolumeTemplates(SystemTest.client).get(0);
+
+        List<ComponentDescriptor> componentDescriptors = new ArrayList<ComponentDescriptor>();
+
+        MachineTemplate machineTemplate = new MachineTemplate();
+        machineTemplate.setName("Machine in system demo222");
+        machineTemplate.setDescription("Machine in system");
+        machineTemplate.setMachineConfigRef(config.getId());
+        machineTemplate.setMachineImageRef(image.getId());
+        machineTemplate.setCredentialRef("#MyCredential");
+        List<MachineTemplate.Volume> volumes = new ArrayList<MachineTemplate.Volume>();
+        MachineTemplate.Volume templateVol = new MachineTemplate.Volume();
+        templateVol.setInitialLocation("/vol");
+        templateVol.setVolumeRef("#MyVolume");
+        volumes.add(templateVol);
+        machineTemplate.setVolumes(volumes);
+
+        ComponentDescriptor component = new ComponentDescriptor();
+        component.setName("MyMachine");
+        component.setQuantity(1);
+        component.setComponentTemplate(machineTemplate);
+        componentDescriptors.add(component);
+
+        component = new ComponentDescriptor();
+        component.setName("MyCredential");
+        component.setQuantity(1);
+        component.setComponentTemplateRef(credTemplate);
+        componentDescriptors.add(component);
+
+        component = new ComponentDescriptor();
+        component.setName("MyVolume");
+        component.setQuantity(1);
+        component.setComponentTemplateRef(volumeTemplate);
+        componentDescriptors.add(component);
+
+        SystemTemplate systemTemplate = new SystemTemplate();
+        systemTemplate.setName("System Demo222");
+        systemTemplate.setDescription("My first system template demo");
+
+        systemTemplate.setComponentDescriptors(componentDescriptors);
+
+        CreateResult<SystemTemplate> result = SystemTemplate.createSystemTemplate(SystemTest.client, systemTemplate);
+
+        String id = result.getResource().getId();
+
+        systemTemplate = SystemTemplate.getSystemTemplateByReference(SystemTest.client, id);
+
+    }
+
+    @Test
+    public void testSystem() throws Exception {
+
+        // retrieve SystemTemplate
+        SystemTemplate systemTemplate = SystemTemplate.getSystemTemplates(SystemTest.client,
+            QueryParams.builder().filter("name='SpringooTemplate2'").build()).get(0);
+
+        // create System
+        SystemCreate systemCreate = new SystemCreate();
+        systemCreate.setName("MySystem1");
+        systemCreate.setDescription("my first system");
+        systemCreate.setSystemTemplateRef(systemTemplate.getId());
+        CreateResult<System> result = System.createSystem(SystemTest.client, systemCreate);
+        if (result.getJob().getState() == Job.State.FAILED) {
+            Assert.fail("Failed to create System: " + result.getJob().getStatusMessage());
+        }
+        System system = result.getResource();
+
+        // wait until System started or stopped
+
+        int tries = 10;
+        while (tries-- > 0) {
+            system = System.getSystemByReference(SystemTest.client, system.getId());
+            if (system.getState() != System.State.CREATING) {
+                break;
+            }
+            Thread.sleep(2000);
+        }
+
+        if (system.getState() != System.State.STARTED && system.getState() != System.State.STOPPED) {
+            Assert.fail("Wrong System state after creation: " + system.getState());
+        }
+
+        // explore System collections
+
+        for (SystemMachine systemMachine : system.getMachines()) {
+            Machine machine = systemMachine.getMachine();
+            java.lang.System.out.println("Machine name=" + machine.getName() + " state=" + machine.getState());
+        }
+
+        for (SystemVolume systemVolume : system.getVolumes()) {
+            Volume volume = systemVolume.getVolume();
+            java.lang.System.out.println("Volume name=" + volume.getName() + " state=" + volume.getState());
+        }
+
+        for (SystemNetwork systemNetwork : system.getNetworks()) {
+            Network network = systemNetwork.getNetwork();
+            java.lang.System.out.println("Network name=" + network.getName() + " state=" + network.getState());
+        }
+
+        // for (SystemAddress systemAddress : system.getAddresses()) {
+        // Address address = systemAddress.getAddress();
+        // java.lang.System.out.println("Address ip=" + address.getIp());
+        // }
+
+        // start System
+
+        if (system.getState() == System.State.STOPPED) {
+            Job job = system.start();
+            if (job.getState() == Job.State.FAILED) {
+                Assert.fail("Failed to start System: " + job.getStatusMessage());
+            }
+            tries = 10;
+            while (tries-- > 0) {
+                Thread.sleep(4000);
+                system = System.getSystemByReference(SystemTest.client, system.getId());
+                if (system.getState() != System.State.STARTING && system.getState() != System.State.MIXED) {
+                    break;
+                }
+            }
+            if (system.getState() != System.State.STARTED) {
+                Assert.fail("Failed to start System: " + system.getState());
+            }
+        }
+
+        // check that System machines are started
+
+        for (SystemMachine systemMachine : system.getMachines()) {
+            Machine machine = Machine.getMachineByReference(SystemTest.client, systemMachine.getMachine().getId());
+            Assert.assertEquals(machine.getState(), Machine.State.STARTED);
+        }
+
+        // stop System
+
+        Job job = system.stop();
+        if (job.getState() == Job.State.FAILED) {
+            Assert.fail("Failed to stop System: " + job.getStatusMessage());
+        }
+        tries = 10;
+        while (tries-- > 0) {
+            Thread.sleep(4000);
+            system = System.getSystemByReference(SystemTest.client, system.getId());
+            if (system.getState() != System.State.STOPPING && system.getState() != System.State.MIXED) {
+                break;
+            }
+        }
+        if (system.getState() != System.State.STOPPED) {
+            Assert.fail("Failed to start System: " + system.getState());
+        }
+
+        // check that System machines are stopped
+
+        for (SystemMachine systemMachine : system.getMachines()) {
+            Machine machine = Machine.getMachineByReference(SystemTest.client, systemMachine.getMachine().getId());
+            Assert.assertEquals(machine.getState(), Machine.State.STOPPED);
+        }
+
+        // delete System
+
+        job = system.delete();
+        if (job.getState() == Job.State.FAILED) {
+            Assert.fail("Failed to delete System: " + job.getStatusMessage());
+        }
+        tries = 10;
+        while (tries-- > 0) {
+            try {
+                system = System.getSystemByReference(SystemTest.client, system.getId());
+            } catch (CimiProviderException e) {
+                break;
+            }
+            if (system.getState() != System.State.DELETING) {
+                Assert.fail("Wrong System state after deleting: " + system.getState());
+                break;
+            }
+            Thread.sleep(2000);
+        }
+        try {
+            system = System.getSystemByReference(SystemTest.client, system.getId());
+        } catch (CimiProviderException e) {
+            return;
+        }
+        Assert.fail("Deleted system is still there !");
+
+    }
+
+    @Test
+    public void createSpringooSystemTemplate() throws Exception {
+
+        MachineConfiguration config = MachineConfiguration.getMachineConfigurations(SystemTest.client,
+            QueryParams.builder().filter("name='small'").build()).get(0);
+        MachineImage image = MachineImage.getMachineImages(SystemTest.client,
+            QueryParams.builder().filter("name='springoo'").build()).get(0);
         Network publicNetwork = Network.getNetworks(SystemTest.client,
-            QueryParams.builder().filter("networkType='PUBLIC'").build()).get(0);
+            QueryParams.builder().filter("name='public network'").build()).get(0);
         NetworkConfiguration privateNetworkConfig = NetworkConfiguration.getNetworkConfigurations(SystemTest.client,
-            QueryParams.builder().filter("networkType='PRIVATE'").build()).get(0);
+            QueryParams.builder().filter("name='privateNetworkConfig'").build()).get(0);
 
         List<ComponentDescriptor> componentDescriptors = new ArrayList<ComponentDescriptor>();
 
@@ -112,6 +384,7 @@ public class SystemTest {
     }
 
     @Test
+    @Ignore
     public void createInvalidSystemTemplateWithWrongNetworkRef() throws Exception {
         MachineConfiguration config = MachineConfiguration.getMachineConfigurations(SystemTest.client).get(0);
         MachineImage image = MachineImage.getMachineImages(SystemTest.client).get(0);
@@ -148,7 +421,7 @@ public class SystemTest {
         try {
             CreateResult<SystemTemplate> result = SystemTemplate.createSystemTemplate(SystemTest.client, systemTemplate);
         } catch (CimiProviderException ex) {
-            System.out.println(ex.getMessage());
+            java.lang.System.out.println(ex.getMessage());
             return;
         }
         Assert.fail("Invalid system template creation did not raise an exception");
